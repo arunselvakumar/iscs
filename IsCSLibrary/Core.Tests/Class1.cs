170b6ad80622b93f4c3c2789b5d73375b4d6d97e
arunselvakumar <b.arunselvakumar@outlook.com>
2016-07-08 18:34:51 +0530

    Added Boilerplate Code.

---
+++ IsCSLibrary/Core.Tests/Class1.cs
@@ -0,0 +1,26 @@
+﻿namespace Core.Tests
+{
+    using Xunit;
+
+    public class Class1
+    {
+        public Class1()
+        {
+            // var foo = Common.IsDBNull(89);
+            var foo = Numbers.IsNumber(45);
+        }
+
+        [Fact]
+        public void IsNumberWorking()
+        {
+            var foo = Numbers.IsNumber("Hello World");
+
+
+            Assert.Equal(true, Numbers.IsNumber(45));
+            Assert.Equal(true, Numbers.IsNumber(45.89));
+            Assert.Equal(true, Numbers.IsNumber(00.89));
+            Assert.Equal(true, Numbers.IsNumber(-90));
+            Assert.Equal(false, Numbers.IsNumber("Hello World"));
+        }
+    }
+}
---
+++ IsCSLibrary/Core/Common/Common.cs
@@ -0,0 +1,15 @@
+﻿namespace Core
+{
+    using System;
+
+    public static class Common
+    {
+        #region Pubic Methods
+
+        public static bool IsDBNull(object value)
+        {
+            return Convert.IsDBNull(value);
+        }
+        #endregion
+    }
+}
